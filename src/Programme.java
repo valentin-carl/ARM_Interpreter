b0342f2cfa8c749ae3ba4ee1bbe7fdd5019a4026
valentin-carl <87319498+valentin-carl@users.noreply.github.com>
2022-02-21 11:38:58 +0100

    Update Programme.java

--- src/Programme.java
+++ src/Programme.java
@@ -26,34 +26,50 @@ public class Programme {
     // runs the programme
     public void run () {
         // TODO write run method
-        for (Instruction i : this.code) i.execute();
+        //for (Instruction i : this.code) i.execute();
 
         int numberOfLines = this.code.size();
         // assume: every line number is incremented by +4 (in decimal!)
         int lineIndex = 0;
+        whileLoop:
         while (lineIndex < numberOfLines) {
 
             // working on this instruction in the current iteration
             Instruction current = this.code.get(lineIndex);
 
+            //System.out.println("CURRENT INSTRUCTION: " + current.lineNo +  ": " + current.type + ";");
+
+            // execute current instruction
+            this.code.get(lineIndex).execute();
+
+            /*
+            if (current.type == INSTRUCTION_TYPE.CMP) {
+                System.out.println("comparing " + ((CmpInstruction) current).r1.value + " and " + ((CmpInstruction) current).r2.value);
+            }
+             */
+
             // if instruction deviates from adding programme counter + 1 (maybe) => jump instructions
             if (current.type == INSTRUCTION_TYPE.B || current.type == INSTRUCTION_TYPE.BEQ || current.type == INSTRUCTION_TYPE.BNE) {
-                System.out.println("hello");
                 switch (current.type) {
                     case B -> {
                         for (int i = 0; i < this.code.size(); i++) {
                             if (this.code.get(i).lineNo == ((BInstruction) current).lineToJumpTo) {
-                                lineIndex = ((BInstruction) current).lineToJumpTo;
-                                break;
+                                //lineIndex = ((BInstruction) current).lineToJumpTo;
+                                lineIndex = this.getLineIndex(((BInstruction) current).lineToJumpTo);
+                                continue whileLoop;
+                                //break;
                             }
                         }
                     }
                     case BEQ -> {
                         if (StatusRegister.singleton.getStatus()) {
+                            //System.out.println("status register = " + StatusRegister.getInstance().getStatus());
                             for (int i = 0; i < this.code.size(); i++) {
                                 if (this.code.get(i).lineNo == ((BeqInstruction) current).lineToJumpTo) {
-                                    lineIndex = ((BeqInstruction) current).lineToJumpTo;
-                                    break;
+                                    //lineIndex = ((BeqInstruction) current).lineToJumpTo;
+                                    lineIndex = this.getLineIndex(((BeqInstruction) current).lineToJumpTo);
+                                    continue whileLoop;
+                                    //break;
                                 }
                             }
                         }
@@ -62,25 +78,41 @@ public class Programme {
                         if (!StatusRegister.singleton.getStatus()) {
                             for (int i = 0; i < this.code.size(); i++) {
                                 if (this.code.get(i).lineNo == ((BneInstruction) current).lineToJumpTo) {
-                                    lineIndex = ((BneInstruction) current).lineToJumpTo;
-                                    break;
+                                    //lineIndex = ((BneInstruction) current).lineToJumpTo;
+                                    lineIndex = this.getLineIndex(((BneInstruction) current).lineToJumpTo);
+                                    continue whileLoop;
+                                    //break;
                                 }
                             }
                         }
                     }
                 }
-            } else {
-                // if reached, current instruction is not affecting the programme counter => boring!
-                lineIndex++;
             }
-        }
 
+            // if reached, current instruction is not affecting the programme counter => boring!
+            lineIndex++;
+
+            /*
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+             */
+        }
+    }
+
+    private int getLineIndex (int lineNo) {
+        for (int lineIndex = 0; lineIndex < this.code.size(); lineIndex++) {
+            if (this.code.get(lineIndex).lineNo == lineNo) return lineIndex;
+        }
+        return -1;
     }
 
     // print registers lists if existing
     public void printRegisters () {
         for (Register r : Register.registers) {
-            System.out.println(r);
+            if (r.used) System.out.println(r);
         }
     }
 }
